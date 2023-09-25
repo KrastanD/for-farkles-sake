5fe81e6e65e25b381ad2357391adc17bb3afb6b3
Krastan Dimitrov <krastan.kdd@gmail.com>
2023-09-25 15:24:53 -0500

    Add a 2 more tests to getValue

--- src/__tests__/getValue.test.mjs
+++ src/__tests__/getValue.test.mjs
@@ -14,4 +14,12 @@ describe("getValue", () => {
   it("returns 350 for a 2 triplet, 1 and 5", () => {
     assert.strictEqual(getValue([1, 2, 2, 2, 5]), 350);
   });
+
+  it("returns 1500 for a straight", () => {
+    assert.strictEqual(getValue([1, 2, 3, 4, 5, 6]), 1500);
+  });
+
+  it("returns 400 for a 3 triple and 2 5s", () => {
+    assert.strictEqual(getValue([5, 5, 3, 3, 3, 4]), 400);
+  });
 });
